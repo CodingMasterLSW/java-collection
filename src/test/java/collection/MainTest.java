ca4138d177b07f86b5d76bc22d91dc369697e5bf
CodingMasterLSW <spqjekdl1004@gmail.com>
2026-04-15 13:30:56 +0900

    chore: init project

---
+++ src/test/java/collection/MainTest.java
@@ -0,0 +1,12 @@
+package collection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    @Test
+    void sanity() {
+        assertEquals(2, 1 + 1);
+    }
+}
